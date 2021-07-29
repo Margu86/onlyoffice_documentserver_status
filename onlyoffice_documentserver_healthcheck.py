40b6495652f3f86284121a53ca1bffa5d24db1b2
Margu <44941663+Margu86@users.noreply.github.com>
2021-07-29 10:36:47 +0200

    added auto-restart if nginx has failed

--- onlyoffice_documentserver_healthcheck.py
+++ onlyoffice_documentserver_healthcheck.py
@@ -1,13 +1,24 @@
 #!/usr/bin/python3
 
-import urllib.request
-
-healthcheck = urllib.request.urlopen("https://docsrv.technetex.net/healthcheck")
-status = healthcheck.read().decode('utf-8')
-if(str(healthcheck.getcode() == '200')):
-    if(status):
-        print('0 "DocSrv" - DocumentServer is running')
+import urllib.request, os
+try:
+    healthcheck = urllib.request.urlopen("https://docsrv.technetex.net/healthcheck")
+    status = healthcheck.read().decode('utf-8')
+    if(str(healthcheck.getcode() == '200')):
+        if(status):
+            print('0 "DocSrv" - DocumentServer is running')
+        else:
+            print('2 "DocSrv" - DocumentServer not running')
     else:
-        print('2 "DocSrv" - DocumentServer not running')
-else:
-    print('3 "DocSrv" - Unknown state')
+        if(os.system('service nginx status') != 0):
+            print('2 "DocSrv" - NGinX not working')
+            os.system('service nginx restart')
+        else:
+            print('3 "DocSrv" - There seems to be a http problem not related to NGinX')
+except:
+    print('3 "DocSrv" - It seems a HTTP error occured')
+    if(os.system('service nginx status') != 0):
+        print('2 "DocSrv" - NGinX not working')
+        os.system('service nginx restart')
+    else:
+        print('3 "DocSrv" - There seems to be a http problem not related to NGinX')
